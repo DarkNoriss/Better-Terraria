080d0f73c3befcdb04fe7767e0aab5f2f2bb69ac
DarkNoriss <darknoriss@gmail.com>
2023-12-14 00:48:08 +0100

    Add main menu scene and button functionality

--- src/scripts/player/player.gd
+++ src/scripts/player/player.gd
@@ -10,35 +10,35 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
-	pass
-  
+  pass
+
 func _process(_delta):
-	pass
+  pass
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+  # Add the gravity.
+  if not is_on_floor():
+    velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+  if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+    velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+  var direction = Input.get_axis("ui_left", "ui_right")
 
-	if direction == -1:
-		get_node("AnimatedSprite2D").flip_h = true
-		
-	elif direction == 1:
-		get_node("AnimatedSprite2D").flip_h = false
-		
-	if direction:
-		anim.play("run")
-		velocity.x = direction * SPEED
-	else:
-		anim.play("idle")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+  if direction == -1:
+    get_node("AnimatedSprite2D").flip_h = true
 
-	move_and_slide()
+  elif direction == 1:
+    get_node("AnimatedSprite2D").flip_h = false
+		
+  if direction:
+    anim.play("run")
+    velocity.x = direction * SPEED
+  else:
+    anim.play("idle")
+    velocity.x = move_toward(velocity.x, 0, SPEED)
+
+  move_and_slide()
