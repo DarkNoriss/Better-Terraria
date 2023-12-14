83ea2307a8b9ab075f1c87d3416a7d2e5a3940be
DarkNoriss <darknoriss@gmail.com>
2023-12-14 01:53:20 +0100

    Refactor player movement and animation logic

--- src/scripts/player/player.gd
+++ src/scripts/player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 @export var SPEED: float = 300.0
 @export var JUMP_VELOCITY: float = -400.0
 
@@ -8,37 +7,54 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction: float
+
 
 func _ready():
-  pass
+	pass
+
 
 func _process(_delta):
-  pass
+	pass
+
 
 func _physics_process(delta):
-  # Add the gravity.
-  if not is_on_floor():
-    velocity.y += gravity * delta
+	setAnimations()
+
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
 	# Handle jump.
-  if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-    velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-  var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 
-  if direction == -1:
-    get_node("AnimatedSprite2D").flip_h = true
+	if direction:
+		velocity.x = direction * SPEED
 
-  elif direction == 1:
-    get_node("AnimatedSprite2D").flip_h = false
-		
-  if direction:
-    anim.play("run")
-    velocity.x = direction * SPEED
-  else:
-    anim.play("idle")
-    velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-  move_and_slide()
+	move_and_slide()
+
+
+func setAnimations():
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
+
+	if is_on_floor():
+		if direction == 0:
+			anim.play("idle")
+
+		else:
+			anim.play("run")
+
+	else:
+		anim.play("jump")
