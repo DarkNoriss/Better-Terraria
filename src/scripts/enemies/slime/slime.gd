7e6d267c3839b7b721daa5e820b362e194fe706d
Patryk Przybylski <darknoriss@gmail.com>
2023-12-20 00:17:47 +0100

    Update slime enemy behavior and attributes

--- src/scripts/enemies/slime/slime.gd
+++ src/scripts/enemies/slime/slime.gd
@@ -1,22 +1,22 @@
 extends CharacterBody2D
 
-var health: int
-var damage: int
-var coins: int
+var health: int = 42
+var damage: int = 18
+var coins: int = 7
 
-var jump_velocity: float
-var gravity: float
+var speed: float = 200
+var jump_velocity: float = 600
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = get_node("AnimationPlayer")
 
+var player
+var jumpTimer: float = 2.0
+var jumpDirection: String = "left"
+
 
 func _ready():
-	health = 42
-	damage = 18
-	coins = 7
-
-	jump_velocity = 400
-	gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	player = get_node("../Player")
 
 	anim.play("idle")
 
@@ -29,6 +29,26 @@ func _process(_delta):
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+		if jumpDirection == "right":
+			velocity.x = speed
+		elif jumpDirection == "left":
+			velocity.x = -speed
+
+	if is_on_floor():
+		jumpTimer -= delta
+
+		velocity.x = 0
+
+		if jumpTimer <= 0.0:
+			jumpTimer = 2.0
+
+			if player.position.x > position.x:
+				jumpDirection = "right"
+			elif player.position.x < position.x:
+				jumpDirection = "left"
+
+			velocity.y -= 500
 
 	move_and_slide()
 
